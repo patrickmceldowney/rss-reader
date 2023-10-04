742d999532c6e5f95dbf46e3f0b8620582356cd8
--replace-all <ptmceldowney@gmail.com>
2023-10-03 23:09:26 -0500

    saving progres

--- rssManager.rb
+++ rssManager.rb
@@ -2,8 +2,8 @@ require 'open-uri'
 require 'nokogiri'
 require_relative 'rssData.rb'
 
-class RSSManager
-  def loadRSSChannelFromUrl(rssUrl)
+class RssManager
+  def loadRssChannelFromUrl(rssUrl)
     # Read RSS from URL
     xmlDoc = Nokogiri::XML(open(rssUrl))
 
@@ -11,9 +11,29 @@ class RSSManager
     rssChannelNode = xmlDoc.root.at_xpath('channel')
 
     # Create new RSS channel
-    rssChanne = RssChannel.new
+    rssChannel = RssChannel.new
     rssChannel.Title = xmlDoc.root.at_xpath('channel/title').content
+    rssChannel.Description = rssChannelNode.at_xpath('description').content
+    rssChannel.Link = rssChannelNode.at_xpath('link').content
+    rssChannel.PubDate = rssChannelNode.at_xpath('pubDate').content
+    rssChannel.RssItems = loadRssItemsFromUrl(rssUrl)
 
     return rssChannel
+  end
+end
+
+def loadRssItemsFromUrl(rssUrl)
+  # Read RSS from URL
+  xmlDoc = Nokogiri::XML(open(rssUrl))
+
+  # Get XML Nodes
+  rssItemNodes = xmlDoc.root.at_xpath('channel/item')
+
+  # Store RSSitems temporarily
+  rssItems = []
+
+  for index in 0..rssItemNodes.length - 1 do
+    # Create new RSSItem
+    rssItem = RssItem.new
   end
 end
